ca20e422fd5520a5bc0d1c75fb179f07144bc9bf
Saqib Javed <jvedsaqib6291@gmail.com>
2023-10-10 00:03:16 +0530

    lin search

--- SEARCH/Linear_Search.cpp
+++ SEARCH/Linear_Search.cpp
@@ -4,5 +4,28 @@ using namespace std;
 
 int main(){
     vector<int> arr;
-    cout << "Hello";
+    int n;
+    cout << "Enter length : ";
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        int number;
+        cout << "Enter " << i <<"th element : ";
+        cin >> number;
+        arr.push_back(number);
+    }
+
+    int key;
+
+    cout << "Enter the key : ";
+    cin >> key;
+
+    for(int i = 0; i < n; i++){
+        if(arr[i] == key){
+            cout << "Element found!!!";
+            return 0;
+        }
+    }
+    cout << "Element not found!!!";
+
 }
